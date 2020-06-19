1b70f03e78f9a4e78421b08ebe4a1a8479340ddc
amaankahmad <65362744+amaankahmad@users.noreply.github.com>
2020-06-19 10:37:32 +0100

    Add files via upload

--- DifferentionFromFirstPrinciples/MyDifferentiationFunc.m
+++ DifferentionFromFirstPrinciples/MyDifferentiationFunc.m
@@ -1,13 +1,12 @@
 function [Y,step] = MyDifferentiationFunc(Fx,xValue)
 % Differentiation of Fx from first principles
 h=1;
-syms x;
 fpeq=(Fx(xValue+h)-Fx(xValue))/h; % Differentiation from first principles equation
 current=fpeq;
 previous=0;
-while abs(current-previous)>=0.0001 % Precision of 4dp reached
+while abs((current-previous)/previous)>=0.0001 % Precision of 4dp reached
     previous=current; % Store value before change
-    h=h*0.8; % Step change - decreasing h by 20%
+    h=h*0.1; % Step change - decreasing h by x10
     current=fpeq; % Current value from equation
 end
 Y=previous; % Value of first derivative
--- DifferentionFromFirstPrinciples/mydiffunctest.m
+++ DifferentionFromFirstPrinciples/mydiffunctest.m
@@ -1,17 +1,6 @@
 xValue=1000;
-Fx=sqrt(xValue);
+Fx=@(x) sqrt(x);
 % Differentiation of Fx from first principles
-h=1;
-syms x;
-fpeq=(Fx*(xValue+h)-Fx*(xValue))/h; % Differentiation from first principles equation
-current=fpeq;
-previous=0;
-while abs(current-previous)>=0.0001 % Precision of 4dp reached
-    previous=current; % Store value before change
-    h=h*0.8; % Step change - decreasing h by 20%
-    current=fpeq; % Current value from equation
-end
-disp(previous); % Value of first derivative
-disp(h); % Step size
-
+[value,step]=MyDifferentiationFunc(Fx,xValue);
+disp([value,step]);
 
