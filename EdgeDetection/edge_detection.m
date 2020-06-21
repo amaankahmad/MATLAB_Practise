ec4aff0e4393d2f6ad5b814848d7b1425de96ecc
amaankahmad <65362744+amaankahmad@users.noreply.github.com>
2020-06-21 20:59:49 +0100

    Add files via upload

---
+++ EdgeDetection/edge_detection.m
@@ -0,0 +1,20 @@
+function [outputImage] = edge_detection(inputImage)
+% Initialise the size of the inputimage
+[rows , cols] = size(inputImage);
+% Intialise size of outputImage and equation outputs as empty
+outputImage=uint8(zeros([rows cols]));
+outV=uint8(zeros([rows cols]));
+outH=uint8(zeros([rows cols]));
+% Iterating through the rows
+for x=1:1:rows-1
+    % Iterating through the columns
+    for y=1:1:cols-1
+        % Equations for the Veritcal and Horizontal edge detection
+        outV(x,y) = abs(inputImage(x,y)-inputImage(x+1,y));
+        outH(x,y) = abs(inputImage(x,y)-inputImage(x,y+1));
+        % Output image
+        outputImage(x,y) = (outH(x,y)+outV(x,y))/2;
+        
+    end
+end
+end
---
+++ EdgeDetection/edge_detection_test.m
@@ -0,0 +1,17 @@
+clear all
+% Load the image
+load mri % Demo image within MATLAB
+test_image = D(:,:,:,1);
+% Rotate the image by pi/4 radians using the function
+edgeDetectedImage = edge_detection(test_image, pi/4);
+% Display rotated image and original image side-by-side
+figure(1) % Create figure
+subplot(1,2,1)
+imagesc(test_image)
+title('Original image')
+axis square
+subplot(1,2,2)
+imagesc(rotated_image)
+title('Edge image')
+axis square
+colormap gray % Change the colourmap to gray
